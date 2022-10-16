0773b5f132eb03092068f6330812d7a5990de5ad
Naveen Kumar sahu <84307098+Naveens24@users.noreply.github.com>
2022-10-16 12:56:20 +0530

    Update Quadraic_eqn.c

--- Quadraic_eqn.c
+++ Quadraic_eqn.c
@@ -10,18 +10,18 @@ int main() {
     if (discriminant > 0) {
         r1 = (-b + sqrt(discriminant)) / (2 * a);
         r2 = (-b - sqrt(discriminant)) / (2 * a);
-       printf ("The real roots = %.1f %.1f", r1, r2);
+       printf ("The real roots are = %.1f" " and " "%.1f", r1, r2);
     }
 
     else if (discriminant == 0) {
         r1 = r2 = -b / (2 * a);
-         printf ("roots are equal =%.1f %.1f", r1, r2);
+         printf ("roots are equal =%.1f and  %.1f", r1, r2);
     }
 
     else {
         realPart = -b / (2 * a);
         imagPart = sqrt(-discriminant) / (2 * a);
-        printf("root1 = %.1lf+%.1lfi and root2 = %.1f-%.1fi", realPart, imagPart, realPart, imagPart);
+        printf(" Imaginary root1 = %.1f + (%.1f)i and root2 = %.1f - (%.1f)i", realPart, imagPart, realPart, imagPart);
     }
 
     return 0;
