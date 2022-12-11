f1fc6891a13218d543a3bea5844417b74e503c13
Naveen Kumar sahu <84307098+Naveens24@users.noreply.github.com>
2022-12-11 21:55:04 +0530

    Update Decending.c

--- Decending.c
+++ Decending.c
@@ -29,7 +29,7 @@
  
         }
  
-        printf("The numbers arranged in ascending order are given below \n");
+        printf("The numbers arranged in descending order are given below \n");
         for (i = 0; i < n; ++i)
             printf("%d ", arr[i]);
  
